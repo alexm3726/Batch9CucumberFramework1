cc3e11331a7fefa6780ed2d59e6212c4a7cb6bc4
alexm3726 <alexm3726@gmail.com>
2021-08-16 05:11:10 -0400

    first class Rest Assured

--- src/test/java/API/HardCodedExamples.java
+++ src/test/java/API/HardCodedExamples.java
@@ -3,9 +3,7 @@ package API;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-
 import static io.restassured.RestAssured.*;
-
 import org.junit.Test;
 
 public class HardCodedExamples {
@@ -26,8 +24,8 @@ public class HardCodedExamples {
 		String baseURI = RestAssured.baseURI = "http://hrm.syntaxtechs.net/syntaxapi/api";
 
 		RequestSpecification preparedRequest = given().header("Authorization",
-				"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MjYzMDU0ODYsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTYyNjM0ODY4NiwidXNlcklkIjoiMjk2MSJ9.j8ws9_rIGldrI9Urzr_gzCM4rIKQ5hAWU8pbrGoLi0U")
-				.header("Content-Type", "application/json").queryParam("employee_id", "20333320");
+				"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MjkxMDE0NDcsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTYyOTE0NDY0NywidXNlcklkIjoiMzAxMCJ9.wDDfjlZWWnbZ-wMyu-hN6PpKu_iabh8X7WFxBKPMylk")
+				.header("Content-Type", "application/json").queryParam("employee_id", "24241A");
 
 		Response response = preparedRequest.when().get("/getOneEmployee.php");
 
